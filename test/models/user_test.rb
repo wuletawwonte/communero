4970773cffcbe02bbc2d4910767187d60e2b948a
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-22 16:34:15 +0300

    Add validity checks for the User model without some properties

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,27 +2,35 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
 
+  def setup
+    @user = User.new(first_name: "Wuletaw", last_name: "Wonte", email: "wuletaw@gmail.com", password: "12345678")
+  end
+
   test "valid user" do
-    user = users(:one)
-    assert user.valid?
+    assert @user.valid?
   end
 
   test "invalid without first_name" do
-    user = User.new(first_name: "Wuletaw", last_name: "Wonte", email: "wuletawwonte@gmail.com", role: "user", password: "12345678")
-    refute user.valid?
-    assert_not_nil user.errors[:first_name]
+    @user.first_name = nil
+    refute @user.valid?
+    assert_not_nil @user.errors[:first_name]
   end
 
   test "invalid without last_name" do
-    user = User.new(first_name: "Wonte", email: "wuletawwonte@gmail.com", role: "user", password: "12345678")
-    refute user.valid?
-    assert_not_nil user.errors[:last_name]
+    @user.last_name = nil
+    refute @user.valid?
+    assert_not_nil @user.errors[:last_name]
   end
 
   test "invalid without email" do
-    user = User.new(first_name: "Wonte", last_name: "Wonte", role: "user", password: "12345678")
-    refute user.valid?
-    assert_not_nil user.errors[:email]
+    @user.email = nil
+    refute @user.valid?
+    assert_not_nil @user.errors[:email]
+  end
+
+  test "valid without admin boolean value" do
+    @user.admin = nil
+    assert @user.valid?
   end
 
 end
