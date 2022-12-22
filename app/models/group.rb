750fb3d3f830a0f17c461b84fbf50474919011c3
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-22 17:30:07 +0300

    Add unit tests for the Group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :user
+  has_many :members, dependent: :destroy
   has_many :users, through: :members 
+
+  validates :name, presence: true
 end
--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 class GroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @valid_group = groups(:valid)
+    @group_one = groups(:one)
+  end
+
+  test "valid group" do
+    assert @valid_group.valid?
+  end
+
+  test "invalid without name" do
+    @valid_group.name = nil
+    refute @valid_group.valid?
+    assert_not_nil @valid_group.errors[:name]
+  end
+
+  test "valid without description" do
+    @valid_group.description = nil
+    assert @valid_group.valid?
+  end
+
+  test "user has many users" do
+    assert_equal 1, @group_one.users.size 
+  end
+
 end
