873f3a79527eaef92c081f9f8d6dc56cfce12464
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-22 17:45:44 +0300

    Test Comment model with its associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+  has_many :sub_comments, dependent: :destroy
+
+  validates :content, presence: :true
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @valid_comment = comments(:valid)
+    @comment_one = comments(:one)
+  end
+
+  test "valid comment" do
+    assert @valid_comment.valid?
+  end
+
+  test "invalid without content" do
+    @valid_comment.content = nil
+    refute @valid_comment.valid?
+    assert_not_nil @valid_comment.errors[:content]
+  end
+
+  test "comment has many sub comments" do
+    assert_equal 2, @comment_one.sub_comments.size 
+  end
+
 end
