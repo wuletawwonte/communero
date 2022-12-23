c454ef9a6f98883605caef91106a7a61aef6ca5c
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-23 16:13:59 +0300

    Add and configure simple_form gem and its tailwind gem

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}',
     './app/components/**/*.{erb,haml,html,slim}',
+    './config/initializers/**/*',
   ],
   theme: {
     extend: {
