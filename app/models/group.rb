6b718061c7a2e2478488fce196b7dd56d876f669
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-23 18:12:29 +0300

    Show Join button only if the user is not a member of that group

--- app/models/group.rb
+++ app/models/group.rb
@@ -10,12 +10,15 @@
 #  updated_at  :datetime         not null
 #
 class Group < ApplicationRecord
+  after_create :add_member
   belongs_to :user
   has_many :members, dependent: :destroy
   has_many :users, through: :members
   has_many :posts, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
+
+  
 
   def members_count
     "#{self.members.count} Members"
@@ -25,7 +28,21 @@ class Group < ApplicationRecord
     "#{self.posts.count} Posts"
   end
 
-  def created_by(current_user)
-    true if self.user == current_user
+  def created_by(user)
+    true if self.user == user
+  end
+
+  def is_not_member(user_id)
+    if self.members.where(user_id: user_id).blank?
+      return true
+    else
+      return false
+    end
+  end
+
+  private
+
+  def add_member
+    members.create(role: "creator", user:, group: self)
   end
 end
