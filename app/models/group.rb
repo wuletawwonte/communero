5502a6354ccea1979e4f2578c55a8d6c84886aa8
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-23 17:11:32 +0300

    Add created by method in the Group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -16,4 +16,16 @@ class Group < ApplicationRecord
   has_many :posts, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
+
+  def members_count
+    "#{self.members.count} Members"
+  end
+
+  def posts_count
+    "#{self.posts.count} Posts"
+  end
+
+  def created_by(current_user)
+    true if self.user == current_user
+  end
 end
