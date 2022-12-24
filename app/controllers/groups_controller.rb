38f60fa9b75cd470c9959db97a971951928ebb4f
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-24 17:40:09 +0300

    Add kaminari pagination

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,7 @@ class GroupsController < ApplicationController
 
   # GET /groups or /groups.json
   def index
-    @groups = Group.all.order(created_at: :desc)
+    @groups = Group.all.order(created_at: :desc).page params[:page]
   end
 
   # GET /groups/1 or /groups/1.json
