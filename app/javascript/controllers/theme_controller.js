1c6a8c8b53215dd2976687e7b4ed9eba35b14a71
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-24 14:57:51 +0300

    Add the theme toggle logic in the theme controller stimulus function

--- app/javascript/controllers/theme_controller.js
+++ app/javascript/controllers/theme_controller.js
@@ -5,6 +5,26 @@ export default class extends Controller {
   connect() {}
 
   toggleTheme() {
-    this.element.classList.toggle('dark');
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+      if (localStorage.getItem('color-theme') === 'light') {
+        document.documentElement.classList.add('dark');
+
+        localStorage.setItem('color-theme', 'dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('color-theme', 'light');
+      }
+
+      // if NOT set via local storage previously
+    } else {
+      if (document.documentElement.classList.contains('dark')) {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('color-theme', 'light');
+      } else {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('color-theme', 'dark');
+      }
+    }
   }
 }
