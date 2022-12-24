828d6a6c61475c30274bde3c08e5251813b38a34
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-24 20:41:41 +0300

    Fix rubocop linter errors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     can :create, Group
-    can :manage, Group, user: user
+    can :manage, Group, user
     can :manage, Member
     can :manage, Post
     can :manage, Comment
