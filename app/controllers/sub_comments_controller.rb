b5a5c89f5c7808408492baafe37c258d252aa0e0
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-24 16:38:27 +0300

    Change Readmore button style in the posts listing page

--- app/controllers/sub_comments_controller.rb
+++ app/controllers/sub_comments_controller.rb
@@ -1,4 +1,5 @@
 class SubCommentsController < ApplicationController
+  before_action :set_comment
   before_action :set_sub_comment, only: %i[ show edit update destroy ]
 
   # GET /sub_comments or /sub_comments.json
@@ -21,17 +22,23 @@ class SubCommentsController < ApplicationController
 
   # POST /sub_comments or /sub_comments.json
   def create
-    @sub_comment = SubComment.new(sub_comment_params)
+
+    @sub_comment = @comment.sub_comments.build(sub_comment_params)
+    @sub_comment.user = current_user
 
     respond_to do |format|
       if @sub_comment.save
-        format.html { redirect_to sub_comment_url(@sub_comment), notice: "Sub comment was successfully created." }
-        format.json { render :show, status: :created, location: @sub_comment }
+        format.turbo_stream
+        format.html { redirect_to comment_sub_comments_path(@comment), notice: 'Reply added successfully.' }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @sub_comment.errors, status: :unprocessable_entity }
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace("#{helpers.dom_id(@comment)}_form", partial: 'form',
+                                                                                        locals: { comment: @comment })
+        end
+        format.html { render :index, status: :unprocessable_entity }
       end
     end
+
   end
 
   # PATCH/PUT /sub_comments/1 or /sub_comments/1.json
@@ -63,8 +70,13 @@ class SubCommentsController < ApplicationController
       @sub_comment = SubComment.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
-    def sub_comment_params
-      params.fetch(:sub_comment, {})
-    end
+  # Use callbacks to share comment constraints between actions.
+  def set_comment
+    @comment = Comment.includes(:user).find(params[:comment_id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def sub_comment_params
+    params.require(:sub_comment).permit(:content, :comment_id)
+  end
 end
